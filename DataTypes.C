8a5c0f30fc7b0625a8a9a6f678a9541578a5d285
Guney Unay <guneymarketing17@hotmail.com>
2023-11-06 17:39:01 +0300

    data types lesson

---
+++ DataTypes.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    /*
+    int (4bytes) | ( %d )
+    char (1bytes) | (%c)
+    float (4 bytes) | (%f)
+    double (4 bytes) | (%lf)
+
+    */
+
+    int number = 10;
+    float number2 = 5.56;
+
+    printf(" Birinci Sayim : %d 2.ci Sayim %f", number , number2);
+
+    return 0;
+}
