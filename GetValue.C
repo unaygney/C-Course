27b5a31a24b6c9d9844ae759bc1d133239dca706
Guney Unay <guneymarketing17@hotmail.com>
2023-11-07 16:13:18 +0300

    getting value

--- GetValue.C
+++ GetValue.C
@@ -1,12 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    int sayi;
-    printf("Lutfen bir sayi giriniz: ");
-    scanf("%d", &sayi);
-    printf("Aldigimiz sayi %d", sayi);
+    int ilksayim;
+    float ikincisayim;
+    char basharf;
+
+    printf("Lütfen ilk sayiyi giriniz\n");
+    scanf("%d", &ilksayim);
+
+    printf("Lütfen Ikinci sayiyi giriniz.");
+    scanf("%f", &ikincisayim);
+
+    printf("Lütfen Isminizin bas harfini giriniz.");
+    scanf(" %c", &basharf);
+
+    printf("Ilk Sayi : %d Ikinci Sayi : %f Isminizin Bas Harfi : %c", ilksayim, ikincisayim, basharf);
 
     return 0;
 }
