82022580796e4ffb66f1a32e02cdb2c9ef96e1da
Guney Unay <guneymarketing17@hotmail.com>
2023-11-19 14:26:29 +0300

    get data from user for array

--- Arrays.c
+++ Arrays.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
+    //    int dizi [5] = {1,3,5,7,10};
 
-   int dizi [5] = {1,3,5,7,10};
-    
-    printf("Dizinin Elemanları : \n");
-    for(int i = 0; i < 5 ; i++){
-        printf("%d \n",dizi[i]);
-    } 
+    //     printf("Dizinin Elemanları : \n");
+    //     for(int i = 0; i < 5 ; i++){
+    //         printf("%d \n",dizi[i]);
+    //     }
+
+    int ogrenci_tablosu[5][8];
+    int i, j;
+
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            printf("%d no'lu ogrencinin", (i + 1));
+            printf("%d no'lu sinavi : ", (j + 1));
+            scanf("%d", &ogrenci_tablosu[i][j]);
+        }
+    }
 
     return 0;
 }
