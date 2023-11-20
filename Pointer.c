8f6913eef27b68fce9384418e3673feafcb7800f
Guney Unay <guneymarketing17@hotmail.com>
2023-11-20 16:27:48 +0300

    pointer example 2

--- Pointer.c
+++ Pointer.c
@@ -2,13 +2,26 @@
 
 int main()
 {
-    int can = 10;
-    int *p;
+    // int c = 23;
+    // int *p;
 
-    p = &can;
+    // p = &c;
 
-    printf("%d", p);
+    // printf("%d", &c);
+
+    int *ad1, *ad2, *ad;
+    int n = 10, p = 20;
+
+    ad1 = &n;
+    ad2 = &p;
+
+    printf("Atamadan önce ad1 degeri %d \n", *ad1);
+    printf("Atamadan önce ad1 pointer degeri: %d \n", ad1);
+
+    *ad1 = *ad2 + 2;
+
+    printf("Atamadan önce ad1 degeri %d \n", *ad1);
+    printf("Atamadan önce ad1 pointer degeri: %d \n", ad1);
 
     return 0;
-    
 }
